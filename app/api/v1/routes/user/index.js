2c1d7a1d04a3783189a57cfceb1c4f3a346196c4
parsamanesh <stooormix@gmail.com>
2024-04-05 22:48:06 +0330

    create core api for user and get all information about user create find user api for search user page

--- app/api/v1/routes/user/index.js
+++ app/api/v1/routes/user/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { validationResult } = require("express-validator");
 
 const User = require("../../../models/user");
-const { generateJWT } = require("../../../jwt/generate");
+const { generateJWT , decodeJWT } = require("../../../jwt/generate");
 const recaptchaValidator = require("../../../middlewares/recaptchaValidator");
 const { registerSchema ,loginSchema } = require("../../../validators/userValidationSchemas");
 
@@ -37,7 +37,7 @@ router.post("/signup", registerSchema, async (req, res) => {
          return res.status(201).json({
           token:createUser.token,
           username:createUser.username,
-          quick_id:createUser.quickerID
+          quick_id:createUser.quick_id
         });
       } catch (error) {
          return res.status(500).json({
@@ -48,7 +48,6 @@ router.post("/signup", registerSchema, async (req, res) => {
       return res.status(500).json({ message: "Error creating user:" + error });
    }
 });
-
 
 router.post("/login", loginSchema, async (req, res) => {
    try {
@@ -68,7 +67,7 @@ router.post("/login", loginSchema, async (req, res) => {
        return res.status(200).json({
          token:getUserData.token,
          username:getUserData.username,
-         quick_id:getUserData.quickerID
+         quick_id:getUserData.quick_id
        });
      } catch (error) {
        return res.status(500).json({
@@ -81,29 +80,56 @@ router.post("/login", loginSchema, async (req, res) => {
 });
 
 
-router.post("/get-users", loginSchema, async (req, res) => {
-   try {
-     const { username } = req.body;
-     try {
-       var getUserData = await User.findOne({ username });
+router.post("/get-user", loginSchema, async (req, res) => {
+  try {
+    const { quick_id } = req.body;
+    try {
+      var getUserData = await User.findOne({ quick_id });
 
-       if ( !!!getUserData ) {
-         return res.status(400).json({
-           message: "Username not found",
-         });
-       }
+      console.log("user data is:",getUserData)
+      if ( !!!getUserData ) {
+        return res.status(400).json({
+          message: "Quick ID not found",
+        });
+      }
 
-       return res.status(200).json({
-         username:getUserData.username
-       });
-     } catch (error) {
-       return res.status(500).json({
-         message: "Internal server Error, Please try again later",
-       });
-     }
-   } catch (error) {
-     return res.status(500).json({ message: "Error Find user:" + error });
-   }
+      return res.status(200).json({
+        username:getUserData.username,
+        quick_id:getUserData.quick_id
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Internal server Error, Please try again later",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error Find user:" + error });
+  }
 });
+
+router.get("/core",async(req, res)=>{
+  try {
+    const userToken = req.headers.authorization?.split?.(' ')[1];
+    if(!userToken){
+      return res.redirect(401).json({ message: "Token isn't valid"});
+    }
+
+    const decodeData = decodeJWT(userToken)
+    const username = decodeData?.username
+    var getUserData = await User.findOne({ username });
+    if(getUserData){
+      return res.status(200).json({
+        token:getUserData.token,
+        username:getUserData.username,
+        quick_id:getUserData.quick_id
+      })
+    }
+  } catch (error) {
+    console.log("catch error is:",error)
+  }
+
+
+  return res.status(404).json({ message: "user not found"});
+})
 
 module.exports = router;
