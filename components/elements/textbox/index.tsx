edaa301d7157519b55abda4f339718e1fbdca085
parsamanesh <stooormix@gmail.com>
2024-04-05 22:36:56 +0330

    handle number value in text box element adding two type for btn (Text&Primary)

--- components/elements/textbox/index.tsx
+++ components/elements/textbox/index.tsx
@@ -1,39 +1,55 @@
-"use client"
-import React from 'react'
-import TextBoxInterface from './interface'
+"use client";
+import React from "react";
+import TextBoxInterface from "./interface";
 
-const helpers = require("./helpers")
-const {getColor} = helpers
+const helpers = require("./helpers");
+const { getColor } = helpers;
 
 export default function TextBox(props: TextBoxInterface) {
+   const positionType = (() => {
+      let getPosition = (success?: string, error?: string) =>
+         success ? "success" : error ? "error" : undefined;
+      let type: "success" | "error" | undefined = getPosition(
+         props.success,
+         props.error
+      );
 
-    const positionType = (()=>{
-        let getPosition = (success?:string,error?:string)=>success ? "success" : error ? "error" : undefined
-        let type:("success"|"error"|undefined) = getPosition(props.success,props.error)
+      if (type === undefined) return;
 
-        if (type === undefined) 
-            return;
+      let color = getColor(type);
+      let text = props[type];
+      return {
+         color,
+         text,
+      };
+   })();
 
-        let color = getColor(type)
-        let text = props[type]
-        return {
-            color,
-            text
-        }
-    })() 
+   const checkNumberValue = (e: any) => {
+      if (/(^(?!\d)(?!Backspace).*$)/.test(e.key)) {
+         e.preventDefault();
+      }
+   };
 
-    return (
-        <div className={`${props.parentClassName} relative`}>
-            <input 
-                type={props.type}
-                onChange={e => props.getValue?.(e)}
-                style={{outlineColor:positionType?.color}}
-                placeholder={props.placeholder}
-                className={`${props.className} w-full text-white outline-none bg-shark py-3 px-4 rounded placeholder-cadetGrey outline-1 text-base tablet:text-lg`}
-                name={props.name}
-            />
+   return (
+      <div className={`${props.parentClassName} relative`}>
+         <input
+            type={"text"}
+            autoComplete={props.removeAutoComplete?"off":undefined}
+            maxLength={props.maxLength}
+            onKeyDown={(!!props.number && checkNumberValue) || undefined}
+            onChange={(e) => props.getValue?.(e)}
+            style={{ outlineColor: positionType?.color }}
+            placeholder={props.placeholder}
+            className={` w-full text-white outline-none bg-shark py-3 px-4 rounded placeholder-cadetGrey outline-1 text-base tablet:text-lg ${props.className}`}
+            name={props.name}
+         />
 
-            <p style={{color:positionType?.color}} className=' absolute left-0 -bottom-5 pt-1 text-xs opacity-60 transition-opacity '>{positionType?.text}</p>
-        </div>
-    )
+         <p
+            style={{ color: positionType?.color }}
+            className=" absolute left-0 -bottom-5 pt-1 text-xs opacity-60 transition-opacity "
+         >
+            {positionType?.text}
+         </p>
+      </div>
+   );
 }
